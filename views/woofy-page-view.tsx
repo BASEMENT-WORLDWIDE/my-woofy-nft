1c8910b696db1ca6478de6c771ffeab82e5c9073
Austin Buckler <austin@lionhat.co>
2023-12-03 02:41:08 -0600

    added name and backstory fields

--- views/woofy-page-view.tsx
+++ views/woofy-page-view.tsx
@@ -16,9 +16,40 @@ export async function WoofyPageView({ rarity }: WoofyPageViewProps) {
     notFound();
   }
   return (
-    <div>
-      <Image src={woofy.imageUrl} width={256} height={256} alt={woofy.name} />
-      <h1>{woofy.name}</h1>
+    <div className="container mx-auto">
+      <aside className="max-w-xs flex flex-col gap-6">
+        <Image
+          src={woofy.imageUrl}
+          width={256}
+          height={256}
+          alt={woofy.name}
+          className="rounded-2xl w-full h-auto"
+        />
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name" className="block text-sm">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter a name"
+              defaultValue={woofy.name}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="backstory" className="block text-sm">
+              Backstory
+            </label>
+            <textarea
+              id="backstory"
+              name="backstory"
+              placeholder="Enter a backstory"
+            ></textarea>
+          </div>
+        </form>
+      </aside>
     </div>
   );
 }
