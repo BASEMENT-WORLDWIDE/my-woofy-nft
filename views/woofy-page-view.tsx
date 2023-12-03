d3944e134cecdc2ef5d0a95b2daba71fe9ac02c8
Austin Buckler <austin@lionhat.co>
2023-12-03 02:44:09 -0600

    added view on hyperspace link on woofy page.

--- views/woofy-page-view.tsx
+++ views/woofy-page-view.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { db } from "~/db";
 
@@ -25,6 +26,15 @@ export async function WoofyPageView({ rarity }: WoofyPageViewProps) {
           alt={woofy.name}
           className="rounded-2xl w-full h-auto"
         />
+        {woofy.tokenId !== 0 && (
+          <Link
+            href={`https://avax.hyperspace.xyz/collection/avax/5ad14893-3f7e-4be2-9205-d2122591c9f2?tokenAddress=0xbacd77ac0c456798e05de15999cb212129d90b70_${woofy.tokenId}`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            View on Hyperspace
+          </Link>
+        )}
         <form className="flex flex-col gap-4">
           <div className="flex flex-col gap-2">
             <label htmlFor="name" className="block text-sm">
