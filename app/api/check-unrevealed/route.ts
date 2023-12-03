12b0a22e930de81ab3b3486dbf23dfe42f990c01
Austin Buckler <austin@lionhat.co>
2023-12-03 01:44:05 -0600

    reduced calls to avax

--- app/api/check-unrevealed/route.ts
+++ app/api/check-unrevealed/route.ts
@@ -6,20 +6,36 @@ import { WoofyContract } from "~/models/woofy/contract";
 
 export const runtime = "edge";
 
+async function* fetchTokenRarity(rarities: number[]) {
+  for (const rarity of rarities) {
+    try {
+      const response = await WoofyContract.getTokenIdForRarity(rarity);
+      yield response;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
 export const GET = async (req: NextRequest) => {
   const unrevealedWoofys = await db.query.woofysTable.findMany({
     where(fields, ops) {
       return ops.eq(fields.tokenId, 0);
     },
+    columns: {
+      rarity: true,
+    },
   });
-  const unrevealedWoofyTokenIds = await Promise.all(
-    unrevealedWoofys.map((woofy) =>
-      WoofyContract.getTokenIdForRarity(woofy.rarity)
-    )
-  );
-  const revealedWoofyTokenIds = unrevealedWoofyTokenIds.filter(
-    (woofy) => woofy.tokenId !== 0
-  );
+  const revealedWoofyTokenIds: { rarity: number | string; tokenId: number }[] =
+    [];
+  for await (const woofy of fetchTokenRarity(
+    unrevealedWoofys.map((woofy) => woofy.rarity)
+  )) {
+    if (woofy.tokenId === 0) {
+      continue;
+    }
+    revealedWoofyTokenIds.push(woofy);
+  }
   revealedWoofyTokenIds.forEach(async (woofy) => {
     const rarity = (() => {
       if (typeof woofy.rarity === "string") {
